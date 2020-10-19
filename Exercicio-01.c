e63a38c7f5193b7f6ef38293ed2429016b3a32cd
Paulo Rodrigo <paulofigueiredo@id.uff.br>
2020-10-19 19:06:01 -0300

    Initial commit

---
+++ Exercicio-01.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+   Faça um programa que receba dois números inteiros, calcule e imprima a divisão
+   do primeiro pelo segundo.
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, result;
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    
+    result = num1/num2;
+    
+    printf("O resultado da divisao eh %d", result);
+    
+
+    return 0;
+}
